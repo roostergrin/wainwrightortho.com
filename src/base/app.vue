218c3e4af9dbf7858676e0559a06e31731dc5637
matthewlaguardia@gmail.com <matthewlaguardia@gmail.com>
2018-10-30 15:45:35 -0700

    updates(app.vue): adds dispatches

--- src/base/app.vue
+++ src/base/app.vue
@@ -4,7 +4,15 @@
 import NavigationDesktop from 'components/navigation/navigation-desktop/navigation-desktop'
 
 export default {
-  name: 'App',
+  async created () {
+    this.$store.dispatch('GET_PAGES')
+    this.$store.dispatch('GET_APP')
+    this.$store.dispatch('VIEW_NAV', false)
+    this.$store.dispatch('VIEW_BODY', false)
+    this.$store.dispatch('VIEW_TYPES', 0)
+    this.$store.dispatch('VIEW_SHOWMODAL', false)
+    this.$store.dispatch('VIEW_MODALCONTENT', false)
+  },
   components: {
     NavigationDesktop
   }
