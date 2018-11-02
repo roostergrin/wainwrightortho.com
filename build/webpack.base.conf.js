f3fbbe84f00a19bc7ae854dc977b09833a9acffa
spragala <me@spragala.com>
2018-11-02 09:22:11 -0700

    fix(build): adds inline svg loader to correctly display icons

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -58,7 +58,7 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
@@ -84,6 +84,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader?classPrefix'
       }
     ]
   },
