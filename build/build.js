42effb557ae1f43bd4b559362fe661879f94cc2b
Matt Sprague <me@spragala.com>
2019-03-05 10:20:10 -0800

    chore(app): updates sass files for initial project setup

--- build/build.js
+++ build/build.js
@@ -28,14 +28,13 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }) + '\n\n')
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log(chalk.red('  👏 Build 👏 failed 👏 with 👏 errors.\n'))
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.cyan('  🙌 Build complete.\n'))
     console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
+      `  This is Neo. I'm stuck in the Matrix... \n`
     ))
   })
 })
