18b3bfdfd965aae73422f148c13b4f7961898908
Matt Sprague <me@spragala.com>
2019-03-07 11:37:39 -0800

    creates(welcome): creates welcome component / WIP mobile responsize

---
+++ src/components/sections/welcome/welcome.vue
@@ -0,0 +1,11 @@
+<template lang="pug" src="./welcome.pug"></template>
+
+<script>
+export default {
+  computed: {
+    props () {
+      return this.$store.state.pages.welcome
+    }
+  }
+}
+</script>
