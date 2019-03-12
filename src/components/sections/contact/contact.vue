e0746f6cc2967be6e77440815adb7bf332056588
Matt Sprague <me@spragala.com>
2019-03-12 16:41:21 -0700

    creates(contact): creates contact component

---
+++ src/components/sections/contact/contact.vue
@@ -0,0 +1,11 @@
+<template lang="pug" src="./contact.pug"></template>
+
+<script>
+export default {
+  computed: {
+    props () {
+      return this.$store.state.pages.contact
+    }
+  }
+}
+</script>
