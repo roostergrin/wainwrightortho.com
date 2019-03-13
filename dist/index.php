2621c5a1ab33532c252bc8c9ae65c723ceb54ddb
Matt Sprague <me@spragala.com>
2019-03-13 16:56:10 -0700

    feat(map): creates map and adds it to contact page

--- dist/index.php
+++ dist/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css' rel='stylesheet' />
     <?php wp_head(); ?>
 </head>
 <body>
--- src/components/sections/contact/contact.vue
+++ src/components/sections/contact/contact.vue
@@ -1,11 +1,49 @@
 <template lang="pug" src="./contact.pug"></template>
 
 <script>
+var mapbox = require('mapbox-gl/dist/mapbox-gl.js')
+
+mapbox.accessToken = 'pk.eyJ1Ijoicm9vc3RlcmdyaW4iLCJhIjoiY2p0N2g1aDNlMHJ3ODQzcDNsOGlqZDR3ZSJ9._jtkyaXWjB_2k9qhXfUd2g'
+
 export default {
   computed: {
     props () {
       return this.$store.state.pages.contact
     }
+  },
+  async mounted () {
+    setTimeout(() => {
+      /* eslint-disable */
+      var map = new mapbox.Map({
+        container: this.$refs.map,
+        style: 'mapbox://styles/mapbox/satellite-v9',
+        interactive: false,
+        center: [-90.238, 41.540],
+        zoom: 9.4,
+        pitch: 60,
+        attributionControl: false
+      })
+
+      this.props.features.forEach((marker) => {
+        let coords = []
+        coords.push(+marker.coordinates.lng, +marker.coordinates.lat)
+
+        let el = document.createElement('div')
+        el.className='contact__marker'
+        el.style.backgroundImage ='url(https://d22y7cgfo0b8n4.cloudfront.net/contact/Location.svg)'
+
+        let popup = new mapbox.Popup({ offset: 35, closeOnClick: false})
+        .setLngLat(coords)
+        .setHTML(marker.popup)
+
+        new mapbox.Marker(el)
+        .setLngLat(coords)
+        .addTo(map)
+        .setPopup(popup)
+        .togglePopup
+      })
+      /* eslint-enable */
+    }, 10)
   }
 }
 </script>
