5e8083cef5b480251ac4c10f88d9d6f72de96593
Matt Sprague <me@spragala.com>
2019-03-18 16:17:47 -0700

    updates(team): creates team section / WIP mobile styles

--- src/components/sections/team/team.vue
+++ src/components/sections/team/team.vue
@@ -2,9 +2,25 @@
 
 <script>
 export default {
+  data () {
+    return {
+      selectedLocation: 0,
+      activeMember: 0,
+      activeRow: 0
+    }
+  },
   computed: {
     props () {
       return this.$store.state.pages.team
+    }
+  },
+  methods: {
+    toggleLocation (i) {
+      this.selectedLocation = i
+    },
+    handleHover (i, k) {
+      this.activeMember = i
+      this.activeRow = k
     }
   }
 }
