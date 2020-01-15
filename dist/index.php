290606add24613667f844fa1e973f131dfaf794c
Eric <austinwiese.acw@gmail.com>
2020-01-15 15:32:09 -0800

    request(index.php): Added fb chat back in - ticket(#28893)

--- dist/index.php
+++ dist/index.php
@@ -7,6 +7,32 @@
     <?php wp_head(); ?>
 </head>
 <body>
+	<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v4.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=install_email
+  page_id="369498553233625"
+  theme_color="#6699cc"
+    logged_in_greeting="Welcome!  Please let us know if you have any questions about our website.  "
+    logged_out_greeting="Welcome!  Please let us know if you have any questions about our website.  ">
+</div>
   <div id='app' role='application'></div>
   <?php wp_footer(); ?>
   <script type="text/javascript">
